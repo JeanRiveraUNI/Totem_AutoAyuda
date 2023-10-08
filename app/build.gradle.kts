dec6252147389a5d1184417d9838bf1994363542
JeanRiveraUNI <114692318+JeanRiveraUNI@users.noreply.github.com>
2023-10-08 01:21:08 -0300

    Creation project base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.totem_autoayuda"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.totem_autoayuda"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
